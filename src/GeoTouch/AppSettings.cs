6b9b9bb6c563edc1da365cee9ef0a92f802b16e0
Geoffrey Huntley <ghuntley@ghuntley.com>
2015-09-13 14:40:54 +1000

    added xamarin insights

---
+++ src/GeoTouch/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GeoTouch
+{
+	public static class AppSettings
+	{
+		public const string XamarinInsightsApiKey = "1a10d12886d6ddab0ad0d6a36e02a142623d1f1c";
+		public const string ApiClientUserAgent = "Refit/https://github.com/ghuntley/geotouch";
+	}
+}
--- src/iOS/Main.cs
+++ src/iOS/Main.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
-
+using Xamarin;
 namespace GeoTouch.iOS
 {
 	public class Application
@@ -12,6 +12,8 @@ namespace GeoTouch.iOS
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
+			Insights.Initialize(AppSettings.XamarinInsightsApiKey);
+
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
